cbc75595103b88ec5a45e95393e56b0a558a5a0a
Sid <shetty.sud@gmail.com>
2014-10-31 16:04:25 -0500

    Added P4

---
+++ 4.py
@@ -0,0 +1,18 @@
+# https://projecteuler.net/problem=4
+
+__author__ = 'sid'
+
+from EulerUtils import *
+
+
+def largest_palindrome_product():
+    largest = 0
+    for i in range(999, 100, -1):
+        for j in range(999, 100, -1):
+            if is_palindrome(i * j):
+                if (i * j) > largest:
+                    largest = i * j
+    print largest
+
+if __name__ == "__main__":
+    largest_palindrome_product()
