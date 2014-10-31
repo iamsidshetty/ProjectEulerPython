6b9d9357c6da4ed49c90ca17bc127bd8e4d1f0e0
Sid <shetty.sud@gmail.com>
2014-10-31 17:14:16 -0500

    Added P5

---
+++ 5.py
@@ -0,0 +1,31 @@
+# https://projecteuler.net/problem=5
+
+__author__ = 'sid'
+
+from EulerUtils import *
+import math
+# Brute Force
+# i = 20
+# while i % 2 != 0 or i % 3 != 0 or i % 4 != 0 or i % 5 != 0 or i % 6 != 0 or i % 7 != 0 or i % 8 != 0 or i % 9 != 0 or
+# i %10 != 0 or i %11 != 0 or i %12 != 0 or i %13 != 0 or i %14 != 0 or i %15 != 0 or i %16 != 0 or i %17 != 0
+# or i %18 != 0 or i %19 != 0 or i %20 != 0:
+# i += 20
+#
+# print i
+
+
+def smallest_multiple(N):
+    primes = seive_of_eratosthenes(N)
+    result = 1
+
+    for i in primes:
+        powers = math.floor(math.log(N, 10) / math.log(i, 10))
+        result *= math.pow(i, powers)
+
+    print int(result)
+
+
+smallest_multiple(20)
+
+
+
