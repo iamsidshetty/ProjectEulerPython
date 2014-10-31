a52aa79dc7c66a58b116b626aef03b2b3d36bc69
Sid <shetty.sud@gmail.com>
2014-10-31 17:27:05 -0500

    Added P6

---
+++ 6.py
@@ -0,0 +1,19 @@
+__author__ = 'sid'
+# https://projecteuler.net/problem=6
+
+import math
+
+
+def sum_square_difference(N):
+    sum_of_squares = 0
+    square_of_sum = 0
+    for i in range(1, N + 1):
+        sum_of_squares += i * i
+        square_of_sum += i
+
+    print int(math.pow(square_of_sum, 2) - sum_of_squares)
+
+
+sum_square_difference(100)
+
+
