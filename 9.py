89a8df2ced47de5c0f23dff726890b6626d12740
Sid <shetty.sud@gmail.com>
2014-11-02 19:29:52 -0600

    Added P9

--- 9.py
+++ 9.py
@@ -4,6 +4,16 @@ __author__ = 'sid'
 
 
 def special_pythagorean_triplet():
+    for c in xrange(2, 1000):
+        for a in xrange(1, c):
+            b = 1000 - a - c
+
+            if a ** 2 + b ** 2 == c ** 2 and a < b < c:
+                    print a * b * c
+
+
+# Does not generate all the triplets
+def special_pythagorean_triplet_euler():
     # a = n^2 - m^2
     # b = 2nm
     # c = n^2 + m^2
@@ -13,10 +23,25 @@ def special_pythagorean_triplet():
         a = (n * n) - (m * m)
         b = 2 * n * m
         c = (n * n) + (m * m)
-        print a, b, c
-        if a + b + c == 1000:
+        if a + b + c == 1000 and a < b < c:
             print a * b * c
-            break
+
+
+def special_pythagorean_triplet_brute_force():
+    for c in xrange(1, 1000):
+        for b in xrange(1, c):
+            for a in range(1, b):
+                if a ** 2 + b ** 2 == c ** 2 and a + b + c == 1000:
+                    print a * b * c
+
+
+def special_pythagorean_triplet_brute_force_alternate():
+    for a in xrange(1, 335):
+        for b in xrange(1, 667):
+            c = 1000 - b - a
+            if a ** 2 + b ** 2 == c ** 2 and a < b < c:
+                print a * b * c
+
 
 if __name__ == "__main__":
     special_pythagorean_triplet()
