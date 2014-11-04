92bcccc61f12fe06cdaf9d5c13ee8bf8bd8caec5
Sid <shetty.sud@gmail.com>
2014-11-04 16:26:17 -0600

    Added P25

---
+++ 25.py
@@ -0,0 +1,21 @@
+__author__ = 'sid'
+# https://projecteuler.net/problem=25
+
+
+def n_digit_fibonacci_number(n):
+    n_1 = 0
+    n_2 = 1
+    count = 0
+    while True:
+        count += 1
+        num = n_1 + n_2
+        if len(str(num)) == n:
+            break
+        n_1 = n_2
+        n_2 = num
+
+    return count + 1
+
+
+if __name__ == "__main__":
+    print n_digit_fibonacci_number(1000)
